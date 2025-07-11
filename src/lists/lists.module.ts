25ed8ae80d42caed05ddc4e590fbeff789fa7792
berdnaski <berdnaski1177@gmail.com>
2025-07-11 08:35:37 -0300

    chore: configuration database sequelize

---
+++ src/lists/lists.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ListsService } from './lists.service';
+import { ListsController } from './lists.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { List } from './entities/list.entity';
+
+@Module({
+  imports: [
+    SequelizeModule.forFeature([List])
+  ],
+  controllers: [ListsController],
+  providers: [ListsService],
+})
+export class ListsModule {}
